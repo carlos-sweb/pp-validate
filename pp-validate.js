e21ace5959da3d2f8f6f5c3ca4a66eb0c1ee58cb
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-08-31 20:56:15 -0400

    Update

--- pp-validate.js
+++ pp-validate.js
@@ -21,6 +21,38 @@
     }
 
 })(this,function( root, exports , _is ){
+
+    var isS = _is.isString , isU = _is.isUndefined;
+
+
+    var createRules = function( rules ){
+      // =======================================================================
+      //
+      var validateArray = {};
+      // =======================================================================
+      //
+      var keyRules = Object.keys( rules );
+      // -----------------------------------------------------------------------
+      // Procedemos a recorrer las reglas
+      for( var i = 0; i < keyRules.length ; i++){
+        var key = keyRules[i];
+        if( isS( rules[ key ] ) ){
+          rulesArray = rules[ key ].split('|');
+          validateArray[ key ] = {};
+          for( var e = 0 ; e < rulesArray.length ; e++ ){
+             var options = rulesArray[e].split(":");
+             var keyOptions = options[0];
+             var valueOptions =  ['min','max','maxlength','minlength'].includes(keyOptions)  ? parseInt(options[1]) : options[1];
+            validateArray[ key ][keyOptions] = isU(valueOptions) ? true : valueOptions ;
+          }
+        }
+
+      }
+      // -----------------------------------------------------------------------
+
+      return validateArray;
+    }
+
     // =========================================================================
     // FUNC HAS
     var has = function( value , key ){
@@ -40,26 +72,19 @@
     // Type
     // Url
 
-
     var validate = function( data , rules ){
-      // =======================================================================
-      //
-      var validateArray = {};
-      // =======================================================================
-      //
+      var rules = createRules( rules );
       var keyRules = Object.keys( rules );
-      // -----------------------------------------------------------------------
-      // Procedemos a recorrer las reglas
-      for( var i = 0; i < keyRules.length ; i++){
+      var error = {};
+      var result = {};
+      for( var i = 0 ; i < keyRules.length ; i++){
         var key = keyRules[i];
-        validateArray[ key ] = null;
+        console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        console.log( rules[key] );
+        console.log( data[key] );
+        console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
       }
-      // -----------------------------------------------------------------------
-      console.log( validateArray );
-      return false;
+      return { valid : true , error : false };
     }
-
     return validate;
-
-
 });
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var validate = require('./pp-validate.js');
 
 var result = validate(
-  {name : "Carlos Illesca"},
-  {name : "string(max:20,min:10),required(false)"}
+  {name : "Carlos Illesca",age:30},
+  {name : "required|string|maxlength:20|minlength:10",age:'required|number|min:29'}
 );
+// required|max:20|min:10|regex:/^.+@.+$/i|no_regex:/^.+@.+$/i
+console.log( result );
